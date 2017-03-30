cb0c0d04b6d54defcc1d295df4039fef67789c55
Sergey Evseev <evseev.srg@gmail.com>
2017-03-29 17:41:17 -0700

    Fix black frames at the end of the video

--- framework/Source/iOS/GPUImageMovieWriter.m
+++ framework/Source/iOS/GPUImageMovieWriter.m
@@ -16,6 +16,7 @@ NSString *const kGPUImageColorSwizzlingFragmentShaderString = SHADER_STRING
  }
 );
 
+static BOOL allowWriteAudio = NO;
 
 @interface GPUImageMovieWriter ()
 {
@@ -279,6 +280,8 @@ NSString *const kGPUImageColorSwizzlingFragmentShaderString = SHADER_STRING
     });
     isRecording = YES;
 	//    [assetWriter startSessionAtSourceTime:kCMTimeZero];
+    
+    allowWriteAudio = NO;
 }
 
 - (void)startRecordingInOrientation:(CGAffineTransform)orientationTransform;
@@ -365,6 +368,10 @@ NSString *const kGPUImageColorSwizzlingFragmentShaderString = SHADER_STRING
 
 - (void)processAudioBuffer:(CMSampleBufferRef)audioBuffer;
 {
+    if (!allowWriteAudio) {
+        return;
+    }
+    
     if (!isRecording || _paused)
     {
         return;
@@ -800,6 +807,7 @@ NSString *const kGPUImageColorSwizzlingFragmentShaderString = SHADER_STRING
             {
                 if (![assetWriterPixelBufferInput appendPixelBuffer:pixel_buffer withPresentationTime:frameTime])
                     NSLog(@"Problem appending pixel buffer at time: %@", CFBridgingRelease(CMTimeCopyDescription(kCFAllocatorDefault, frameTime)));
+                allowWriteAudio = YES;
             }
             else
             {
